ca8e294e56091eb9bb6a4c54c4c33360761b142e
Kacper Żurawik <kaacper181@gmail.com>
2022-12-03 22:43:13 +0100

    Skeleton

---
+++ test/MainMenu.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Colorful;
+using Console = Colorful.Console;
+using System.Drawing;
+using System.Threading;
+namespace test
+{
+    class MainMenu
+    {
+        public static void Menu()
+        {
+            for (; ; )
+            {
+                Console.Clear();
+                WriteLogo();
+                Say("1", "Add");
+                Say("2", "Substract");
+                Say("3", "Multiply");
+                Say("4", "Divide");
+                string option = Console.ReadLine();
+                if (option == "1")
+                {
+                     
+
+                }
+                else if (option == "2")
+                {
+
+                }
+                else if (option == "3")
+                {
+
+                }
+                else if (option == "4")
+                {
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Error! Please choose a valid option!");
+                    Thread.Sleep(1500);
+                }
+            }
+        }
+
+        public static void Say(string prefix, string message)
+        {
+       
+            Console.Write("[");
+            Console.Write(prefix, Color.LawnGreen);
+            Console.WriteLine("] " + message);
+        }
+
+        public static void WriteLogo()
+        {
+            string logo = @"
+
+▄▀█ █▀█ █▀█ █░░ █ █▀▀ ▄▀█ ▀█▀ █ █▀█ █▄░█
+█▀█ █▀▀ █▀▀ █▄▄ █ █▄▄ █▀█ ░█░ █ █▄█ █░▀█
+";
+
+            Console.WriteLine(logo, Color.LawnGreen);
+        }
+    }
+}
+
---
+++ test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "login";
+            //login stuff
+            MainMenu.WriteLogo();
+            Console.WriteLine("Welcome back!");
+            Console.ReadKey(true);
+            MainMenu.Menu();
+        
+        
+        }
+    }
+}
